2321946daaa1c447544eb4ef9e6d58e5162ab530
Hector Hidalgo <marce.753@gmail.com>
2019-09-09 10:50:58 -0400

    Implemented cors support

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_09_07_170122) do
-
+ActiveRecord::Schema.define(version: 2019_09_09_010318) do
 
   create_table "adoptados", force: :cascade do |t|
     t.integer "edad"
@@ -24,13 +22,6 @@ ActiveRecord::Schema.define(version: 2019_09_07_170122) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "calendarios", force: :cascade do |t|
-    t.string "titulo"
-    t.text "descripcion"
-    t.date "fecha"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
   create_table "busco_mi_duenios", force: :cascade do |t|
     t.string "edad"
     t.string "tamanio"
@@ -38,6 +29,14 @@ ActiveRecord::Schema.define(version: 2019_09_07_170122) do
     t.string "genero"
     t.string "raza"
     t.string "telefono"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "calendarios", force: :cascade do |t|
+    t.string "titulo"
+    t.text "descripcion"
+    t.date "fecha"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
